0b92833ff41a6fd8bebfb58af649a1fdb4f90f1f
DeadlyChambers <shanechambers85@gmail.com>
2022-10-09 07:20:14 -0600

    Breaking dotnet into steps

--- libraries/pipeline_config.groovy
+++ libraries/pipeline_config.groovy
@@ -2,7 +2,7 @@ jte {
     allow_scm_jenkinsfile = 'True'
     permissive_initialization = 'True'
     //pipeline_template = 'my-named-template.groovy'
-    reverse_library_resolution = 'True'
+    reverse_library_resolution = 'False'
 }
 template_methods {
     prepare
