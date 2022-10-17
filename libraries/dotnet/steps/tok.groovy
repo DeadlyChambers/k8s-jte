039444e82426ab064a2991e6a293a23f05c4b2a6
DeadlyChambers <shanechambers85@gmail.com>
2022-10-17 02:55:15 -0600

    Simple stages for jnlp

--- libraries/dotnet/steps/tok.groovy
+++ libraries/dotnet/steps/tok.groovy
@@ -1,7 +1,12 @@
+/* groovylint-disable UnnecessaryGString */
 void call() {
     container('jnlp') {
-        stage("Tok") {
-            echo "Running Step Tok"
+        stage('Tok') {
+            echo 'Running Step Tok'
+            sh(script:
+            """#!/bin/bash
+               set -e +o pipefail
+               docker info""")
         }
     }
 }
