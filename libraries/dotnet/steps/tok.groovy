46511a081f7f3b49f90a62d0dea4b4fc48bc72e3
DeadlyChambers <shanechambers85@gmail.com>
2022-10-17 23:04:16 -0600

    Adding git

--- libraries/dotnet/steps/tok.groovy
+++ libraries/dotnet/steps/tok.groovy
@@ -3,9 +3,11 @@ void call() {
     container('no-priv') {
         stage('Tok') {
             echo 'Running Step Tok'
+            checkout scm
             sh(script:
             """#!/bin/bash
                set -e +o pipefail
+               ls -la
                podman build --target test --build-arg APP_VER=6.0.6 --build-arg D_CONFIG=Release . -t ThreeS.Api/6.0.6
                """)
         }
